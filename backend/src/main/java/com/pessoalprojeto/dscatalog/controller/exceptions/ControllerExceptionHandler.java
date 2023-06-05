99aff5e8802718110c953977603c5d9b9399446c
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-06-05 17:49:12 -0300

    Tratando parcialmente a exceção de validação

--- backend/src/main/java/com/pessoalprojeto/dscatalog/controller/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/com/pessoalprojeto/dscatalog/controller/exceptions/ControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -47,6 +48,19 @@ public class ControllerExceptionHandler {
 		err.setPath(request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err) ;
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardErro> database (MethodArgumentNotValidException e, HttpServletRequest request ){
+		StandardErro err = new StandardErro();
+		
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+		err.setError("Validation exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err) ;
 	}
 
 
