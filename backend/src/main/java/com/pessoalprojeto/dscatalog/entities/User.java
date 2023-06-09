e1c388a0324e2fcfeaf8353b5c267ef0c4353890
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-06-09 11:46:15 -0300

    Minimo para fazer o SpringSecurity funcionar

--- backend/src/main/java/com/pessoalprojeto/dscatalog/entities/User.java
+++ backend/src/main/java/com/pessoalprojeto/dscatalog/entities/User.java
@@ -1,9 +1,11 @@
 package com.pessoalprojeto.dscatalog.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -16,32 +18,44 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+/*
+ * A interface UserDetails faz parte do módulo Spring Security e é usada para
+ * representar os detalhes do usuário durante o processo de autenticação e
+ * autorização. Ela fornece informações essenciais sobre o usuário, como nome de
+ * usuário, senha, autorizações (ou seja, os papéis ou permissões do usuário) e
+ * se a conta do usuário está habilitada ou expirada.
+ */
+public class User implements UserDetails, Serializable {
 	private static final long serialVersionUID = 1L;
-	 
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String firstName;
 	private String lastName;
 	@Column(unique = true)
-	/*A anotação @Column(unique = true) no JPA garante que o valor de uma coluna em um banco de dados 
-	seja único. Ao aplicá-la a um campo ou propriedade em uma entidade, evita-se a duplicação desse valor 
-	em outros registros do banco de dados. Isso é especialmente útil para campos que precisam ser exclusivos, 
-	como endereços de e-mail em uma tabela de usuários.*/
+	/*
+	 * A anotação @Column(unique = true) no JPA garante que o valor de uma coluna em
+	 * um banco de dados seja único. Ao aplicá-la a um campo ou propriedade em uma
+	 * entidade, evita-se a duplicação desse valor em outros registros do banco de
+	 * dados. Isso é especialmente útil para campos que precisam ser exclusivos,
+	 * como endereços de e-mail em uma tabela de usuários.
+	 */
 	private String email;
 	private String password;
-	
+
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "tb_user_role",
-	joinColumns = @JoinColumn(name = "user_id"),
-	inverseJoinColumns = @JoinColumn(name = "role_id"))
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
-	
+
 	public User() {
-		
+
 	}
 
 	public User(Long id, String firstName, String lastName, String email, String password) {
@@ -88,7 +102,7 @@ public class User implements Serializable {
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public Set<Role> getRoles() {
 		return roles;
 	}
@@ -114,5 +128,54 @@ public class User implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-	
+   //A interface UserDetails define os seguintes métodos:
+
+	/*getAuthorities(): Retorna uma coleção de objetos GrantedAuthority que representam as autorizações do usuário. 
+	 Essas autorizações podem ser usadas pelo Spring Security para verificar se o usuário tem permissão para acessar um 
+	 determinado recurso.*/
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// A linha está retornando uma coleção de objetos GrantedAuthority que representam as autorizações do usuário.
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority())).collect(Collectors.toList());
+	}
+
+	/*getUsername(): Retorna o nome de usuário do usuário*/
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+
+	/*isAccountNonExpired(): Retorna um booleano indicando se a conta do usuário não está expirada. Se a conta estiver 
+	 expirada, o usuário não poderá fazer login.*/
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	/*isAccountNonLocked(): Retorna um booleano indicando se a conta do usuário não está bloqueada. Se a conta estiver 
+	 bloqueada, o usuário não poderá fazer login.*/
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	/*isCredentialsNonExpired(): Retorna um booleano indicando se as credenciais (ou seja, a senha) do usuário não estão 
+	 expiradas. Se as credenciais estiverem expiradas, o usuário não poderá fazer login.*/
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	/*isEnabled(): Retorna um booleano indicando se a conta do usuário está habilitada. Se a conta estiver desabilitada, 
+	 o usuário não poderá fazer login.*/
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
 }
--- backend/src/main/java/com/pessoalprojeto/dscatalog/services/UserService.java
+++ backend/src/main/java/com/pessoalprojeto/dscatalog/services/UserService.java
@@ -4,12 +4,16 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 //import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,22 +30,30 @@ import com.pessoalprojeto.dscatalog.services.exceptions.DatabaseException;
 import com.pessoalprojeto.dscatalog.services.exceptions.EntityNotFoundExceptions;
 
 @Service
-public class UserService {
+/*
+ * A interface UserDetailsService faz parte do módulo Spring Security e é usada
+ * para recuperar os detalhes do usuário durante o processo de autenticação. Ela
+ * fornece um método principal, loadUserByUsername(), que é responsável por
+ * carregar os detalhes do usuário com base no nome de usuário fornecido.
+ */
+public class UserService implements UserDetailsService {
+	
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	@Autowired
 	private RoleRepository roleRepository;
-	
-	//beans de métodos
+
+	// beans de métodos
 	@Autowired
 	private BCryptPasswordEncoder passwordEncriptar;
-	
+
 	// MÉTODOS
 	@Transactional(readOnly = true)
 	public Page<UserDTO> findAllPaged(Pageable pageable) {
-		
+
 		Page<User> listaUser = repository.findAll(pageable);
 
 		return listaUser.map(x -> new UserDTO(x));
@@ -62,7 +74,7 @@ public class UserService {
 
 		User entity = new User();
 		copyDTOtoEnrity(dto, entity);
-		entity.setPassword( passwordEncriptar.encode(dto.getPassword()) );
+		entity.setPassword(passwordEncriptar.encode(dto.getPassword()));
 		entity = repository.save(entity);
 
 		return new UserDTO(entity);
@@ -117,6 +129,26 @@ public class UserService {
 			entity.getRoles().add(role);
 		}
 
+	}
+	/* A interface UserDetailsService define o seguinte método: */
+
+	/*
+	 * loadUserByUsername(String username): Este método é chamado pelo Spring
+	 * Security quando um usuário tenta fazer login no sistema. O parâmetro username
+	 * representa o nome de usuário fornecido no processo de autenticação. O método
+	 * é responsável por retornar um objeto que implementa a interface UserDetails,
+	 * contendo os detalhes do usuário correspondente ao nome de usuário fornecido.
+	 */
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			logger.error("User não encontrado: " + username);
+			throw new UsernameNotFoundException("Email not foud");
+		}
+
+		logger.info("User encontrado: " + username);
+		return user;
 	}
 
 }
