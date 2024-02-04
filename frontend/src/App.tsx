92053f7c43f4971be8ab7524518629998bb0eaff
Filipe Lima <fhemrique55@gmail.com>
2024-02-04 16:35:30 -0300

    Começando o componente Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
+      <NavBar />
       <h1>Dscatalog</h1>
     </>
   );
