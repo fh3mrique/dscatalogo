4bd55078779d5cc2a0b6682697ec709dd3d3e17d
Filipe Lima <fhemrique55@gmail.com>
2024-02-04 17:19:45 -0300

    Começando a estilizar a Navbar

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,15 +1,16 @@
+import "./styles.css"
 
 const NavBar = () => {
   return (
-    <nav className="bg-primary">
-      <div>
-        <a href="link">
+    <nav className="main-nav bg-primary navbar navbar-expand-md">
+      <div className="container-fluid">
+        <a href="link" className="nav-logo-text">
           <h4>DS Catalog</h4>
         </a>
-        <div>
-          <ul>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="link">HOME</a>
+              <a className="active" href="link">HOME</a>
             </li>
             <li>
               <a href="link">CATÁLOGO</a>
