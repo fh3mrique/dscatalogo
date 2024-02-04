23d460118ac5efcc1406d8efdf95204105614c02
Filipe Lima <fhemrique55@gmail.com>
2024-02-04 17:27:10 -0300

    Menu hamburguer da Navbar

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,5 @@
-import "./styles.css"
+import './styles.css';
+import 'bootstrap/js/src/collapse.js'
 
 const NavBar = () => {
   return (
@@ -7,10 +8,25 @@ const NavBar = () => {
         <a href="link" className="nav-logo-text">
           <h4>DS Catalog</h4>
         </a>
-        <div className="collapse navbar-collapse">
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#dscatalog-navbar"
+          aria-controls="dscatalog-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id='dscatalog-navbar'>
           <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a className="active" href="link">HOME</a>
+              <a className="active" href="link">
+                HOME
+              </a>
             </li>
             <li>
               <a href="link">CATÁLOGO</a>
