b985ce3a9d2c4cdcd2a1d4fcfc5607ccbda9b8d4
Filipe Lima <fhemrique55@gmail.com>
2024-02-05 12:24:11 -0300

    Finalizando a responsividade

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,27 +1,31 @@
-import NavBar from "../../components/NavBar"
-import MainImage from "../../assets/imgs/main-image.svg"
-import './styles.css'
-import ButtonIcon from "../../components/ButtonIcon"
-
+import NavBar from '../../components/NavBar';
+import MainImage from '../../assets/imgs/main-image.svg';
+import './styles.css';
+import ButtonIcon from '../../components/ButtonIcon';
 
 const Home = () => {
   return (
     <>
       <NavBar />
       <div className="home-container">
-         <div className="home-card">
-            <div className="home-content-container">
-                <h1>Conheça o melhor catalogo de Produtos</h1>
-                <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
-                <ButtonIcon/>
+        <div className="home-card">
+          <div className="home-content-container">
+            <div>
+              <h1>Conheça o melhor catalogo de Produtos</h1>
+              <p>
+                Ajudaremos você a encontrar os melhores produtos disponíveis no
+                mercado.
+              </p>
             </div>
-            <div className="home-image-container">
-                <img src={MainImage} alt="" />
-            </div>
-         </div>
+            <ButtonIcon />
+          </div>
+          <div className="home-image-container">
+            <img src={MainImage} alt="" />
+          </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
