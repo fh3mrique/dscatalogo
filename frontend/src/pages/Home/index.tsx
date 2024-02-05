7259afcbea8b970d3936d0b3ea29dbd2a412463c
Filipe Lima <fhemrique55@gmail.com>
2024-02-05 11:21:14 -0300

    Finalizando o layout X-Small

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import NavBar from "../../components/NavBar"
 import MainImage from "../../assets/imgs/main-image.svg"
 import './styles.css'
+import ButtonIcon from "../../components/ButtonIcon"
 
 
 const Home = () => {
@@ -11,6 +12,8 @@ const Home = () => {
          <div className="home-card">
             <div className="home-content-container">
                 <h1>Conheça o melhor catalogo de Produtos</h1>
+                <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
+                <ButtonIcon/>
             </div>
             <div className="home-image-container">
                 <img src={MainImage} alt="" />
