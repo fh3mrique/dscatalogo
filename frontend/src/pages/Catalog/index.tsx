d298ba1762136259136bb56d10f05b87350ac52d
Filipe Lima <fhemrique55@gmail.com>
2024-02-06 05:28:11 -0300

    Mostrando cards na telaCatalog com Bootstrap Grid System

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -5,8 +5,21 @@ const Catalog = () => {
   return (
     <>
       <NavBar />
-      <div className='container my-4'>
-      <ProductCard/>
+      <div className="container my-4">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            <ProductCard />
+          </div>
+        </div>
       </div>
     </>
   );
