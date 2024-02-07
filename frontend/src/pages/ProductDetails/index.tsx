ace58a8c2740aaeed5735422a3e19977ab8f6b89
Filipe Lima <fhemrique55@gmail.com>
2024-02-07 00:21:40 -0300

    ProductDetails HTML

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,6 +1,37 @@
+import ProductPrice from '../../components/ProductPrice'
+import ArrowIcon from '../../assets/imgs/Seta.svg'
+
 const ProductDetails = () => {
   return (
-    <div>Pagina de product details</div>
+    <div className="product-details-container">
+        <div className="product-details-card">
+
+            <div className="goback-container">
+                <img src={ArrowIcon} alt="" />
+                <h2>VOLTAR</h2>
+            </div>
+
+            <div className="row">
+                <div className="col-xl-6">
+                    <div className="img-container">
+                        <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg" alt="" />
+                    </div>
+                    <div className="name-price-container">
+                        <h1>Nome do produto</h1>
+                        <ProductPrice price={2304.04}/>
+                    </div>
+                </div>
+                <div className="col-xl-6">
+                     <div className="description-container">
+                        <h2>Descrição do produto</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore, sit?</p>
+                     </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
   )
 }
 
