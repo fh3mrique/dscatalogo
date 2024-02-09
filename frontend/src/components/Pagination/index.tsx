d1e8bbb7d9bb16b0f7c8e70e426521fbb7ec3d43
Filipe Lima <fhemrique55@gmail.com>
2024-02-09 17:17:13 -0300

    Pagination CSS

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,15 +1,16 @@
 import ArrowIcon from '../../assets/imgs/Seta.svg'
+import './styles.css'
 
 const Pagination = () => {
   return (
     <div className='pagination-container'>
-        <img src={ArrowIcon} alt="" />
-        <div className='pagination-item'>1</div>
+        <img src={ArrowIcon} alt="" className='arrow-previous arrow-inative'/>
+        <div className='pagination-item active'>1</div>
         <div className='pagination-item'>2</div>
         <div className='pagination-item'>3</div>
         <div className='pagination-item'>...</div>
         <div className='pagination-item'>10</div>
-        <img src={ArrowIcon} alt="" />
+        <img src={ArrowIcon} alt="" className='arrow-next arrow-active' />
     </div>
   )
 }
