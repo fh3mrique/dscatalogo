b0ad718c8764217940d2f07e6d1360fe8ec4d682
Filipe Lima <fhemrique55@gmail.com>
2024-02-11 21:59:47 -0300

    Renderizando dinamicamente os cards do catálogo

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,7 +1,7 @@
 import { Product } from 'types/product';
 import ProductCard from '../../components/ProductCard';
 import { Link } from 'react-router-dom';
-import './styles.css'
+import './styles.css';
 import Pagination from '../../components/Pagination';
 import { useEffect, useState } from 'react';
 import { SpringPage } from '../../types/vendor/spring';
@@ -10,83 +10,41 @@ import { BASE_URL } from '../../util/request';
 import axios from 'axios';
 
 const Catalog = () => {
-  
   const [page, setPage] = useState<SpringPage<Product>>();
 
-  useEffect(()=>{
-
+  useEffect(() => {
     const params: AxiosParams = {
       method: 'GET',
       url: `${BASE_URL}/products`,
-      params:{
-        page:0,
-        size:12
-      }
-    }
+      params: {
+        page: 0,
+        size: 12,
+      },
+    };
 
-    axios(params)
-    .then(response =>{
-      setPage(response.data)
-      console.log(page);
-    })
-  }, [])
+    axios(params).then((response) => {
+      setPage(response.data);
+    });
+  }, []);
 
-  const product = {
-    "id": 1,
-    "name": "The Lord of the Rings",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "price": 90.5,
-    "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg",
-    "date": "2020-07-13T20:50:07.123450Z",
-    "categories": [
-        {
-            "id": 2,
-            "name": "Eletrônicos"
-        }
-    ]
-}
   return (
     <>
       <div className="container my-4 catalog-container">
-        <div className='row catalog-title-container'>
+        <div className="row catalog-title-container">
           <h1>Catálogo de produtos</h1>
         </div>
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3">
-            <Link to="/products/1">
-              <ProductCard product={product} />
-            </Link>
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
-            <Link to="/products/1">
-              <ProductCard product={product} />
-            </Link>
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
-            <Link to="/products/1">
-              <ProductCard product={product} />
-            </Link>
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
-            <Link to="/products/1">
-              <ProductCard product={product} />
-            </Link>
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
-            <Link to="/products/1">
-              <ProductCard product={product} />
-            </Link>
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
-            <Link to="/products/1">
-              <ProductCard product={product} />
-            </Link>
-          </div>
-
+          {page?.content.map((product) => (
+            <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
+              <Link to="/products/1">
+                <ProductCard product={product} />
+              </Link>
+            </div>
+          ))}
         </div>
 
-        <div className='row'>
-          <Pagination/>
+        <div className="row">
+          <Pagination />
         </div>
       </div>
     </>
