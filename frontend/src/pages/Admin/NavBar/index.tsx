05b9f32c1af5cd141fc9202467afaa1b3b71d772
Filipe Lima <fhemrique55@gmail.com>
2024-02-29 11:55:37 -0300

    Restrição de conteúdo baseada no papel do usuário

--- frontend/src/pages/Admin/NavBar/index.tsx
+++ frontend/src/pages/Admin/NavBar/index.tsx
@@ -1,24 +1,27 @@
 import { NavLink } from 'react-router-dom';
 import './styles.css';
+import { hasAnyRole } from '../../../util/request';
 const NavBar = () => {
   return (
     <nav className="admin-nav-container">
       <ul>
         <li>
-          <NavLink to="products" className='admin-nav-item'>
+          <NavLink to="products" className="admin-nav-item">
             <p>Produtos</p>
           </NavLink>
         </li>
         <li>
-          <NavLink to="categories" className='admin-nav-item'>
+          <NavLink to="categories" className="admin-nav-item">
             <p>Categorias</p>
           </NavLink>
         </li>
-        <li>
-          <NavLink to="users" className='admin-nav-item'>
-            <p>Usúarios</p>
-          </NavLink>
-        </li>
+        {hasAnyRole(['ROLE_ADMIN']) && (
+          <li>
+            <NavLink to="users" className="admin-nav-item">
+              <p>Usúarios</p>
+            </NavLink>
+          </li>
+        )}
       </ul>
     </nav>
   );
